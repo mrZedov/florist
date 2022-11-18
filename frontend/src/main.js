8ee504b02b8db3b3a49cd92c0ac2478d9d2d4edd
Vitalii Zgura <Vitalii.Zgura@lantec.ua>
2022-11-18 15:51:28 +0100

    -

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,7 @@ import store from "./store";
 import "primeicons/primeicons.css"; //icons
 import "primevue/resources/primevue.min.css"; //core CSS
 import "primevue/resources/themes/saga-blue/theme.css"; //theme
-
+//
 console.log(process.env);
 createApp(App)
   .use(router)
